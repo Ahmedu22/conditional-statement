8d31df4a1b058f0bf29b9457ff65ca8e6a867abc
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-16 09:14:35 +0300

    conditional satatement

---
+++ app.js
@@ -0,0 +1,28 @@
+// // conditional statement
+// // if, else if and else
+// let a = 10;
+// let b = 20;
+// if(a < b){
+//     console.log('correct');
+// }
+// else if (a > b){
+//     console.log('incorrect');
+// }
+// else{
+//     console.log('a is equal to b');
+// }
+
+// let password = 8;
+// if (password === 8){
+//     console.log('welcome')
+// }
+// else if(password <= 8){
+//     console.log('password is too short')
+// }
+
+
+let age = 15;
+
+if(age <=15){
+    console.log("thats a child");
+}
